982b5d7d1cba5067ff6ded3f9617d6470fc9e681
teach310 <okms310@yahoo.co.jp>
2023-11-30 21:54:25 +0900

    add state machine

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,116 @@
 #include <M5Unified.h>
 #include <Avatar.h>
+#include <unordered_map>
 
 using namespace m5avatar;
 
 Avatar avatar;
 
-const Expression expressions[] = {
-  Expression::Angry,
-  Expression::Sleepy,
-  Expression::Happy,
-  Expression::Sad,
-  Expression::Doubt,
-  Expression::Neutral
-};
-const int expressionsSize = sizeof(expressions) / sizeof(Expression);
-int idx = 0;
-
 void setupAvatar()
 {
-  avatar.setScale(0.4);
-  avatar.setPosition(-56, -96);
+    avatar.setScale(0.4);
+    avatar.setPosition(-56, -96);
 
-  // 目と口のサイズを1.5倍にする
-  Face* face = avatar.getFace();
-  face->setLeftEye(new Eye(12, false));
-  face->setRightEye(new Eye(12, true));
-  face->setMouth(new Mouth(50, 90, 6, 60));
-  avatar.init();
+    // 目と口のサイズを1.5倍にする
+    Face *face = avatar.getFace();
+    face->setLeftEye(new Eye(12, false));
+    face->setRightEye(new Eye(12, true));
+    face->setMouth(new Mouth(50, 90, 6, 60));
+    avatar.init();
 }
+
+enum class State
+{
+    Start,
+    Ready,
+    Idle
+};
+
+class BaseStateBehavior
+{
+public:
+    virtual void onEnter() {}
+    virtual void onUpdate() {}
+    virtual void onExit() {}
+};
+
+State currentState;
+State nextState;
+BaseStateBehavior *stateBehavior;
+std::unordered_map<State, BaseStateBehavior *> stateBehaviors;
+
+void setNextState(State state)
+{
+    nextState = state;
+}
+
+void changeState()
+{
+    if (stateBehavior != nullptr)
+    {
+        stateBehavior->onExit();
+    }
+    currentState = nextState;
+    stateBehavior = stateBehaviors[currentState];
+    stateBehavior->onEnter();
+}
+
+class StartStateBehavior : public BaseStateBehavior
+{
+public:
+    void onEnter() override
+    {
+        M5.Display.setTextSize(2);
+        M5.Display.print("Press to start");
+    }
+
+    void onUpdate() override
+    {
+        M5.update();
+        if (M5.BtnA.wasPressed())
+        {
+            setNextState(State::Ready);
+        }
+    }
+};
+
+class ReadyStateBehavior : public BaseStateBehavior
+{
+public:
+    void onEnter() override
+    {
+        setupAvatar();
+    }
+};
+
+class IdleStateBehavior : public BaseStateBehavior
+{
+public:
+    void onEnter() override
+    {
+        avatar.setExpression(Expression::Happy);
+    }
+};
 
 void setup()
 {
-  auto cfg = M5.config();
-  M5.begin(cfg);
-  setupAvatar();
+    auto cfg = M5.config();
+    M5.begin(cfg);
+    currentState = State::Start;
+    nextState = State::Start;
+    StartStateBehavior *startStateBehavior = new StartStateBehavior();
+    stateBehavior = startStateBehavior;
+    stateBehaviors[State::Start] = startStateBehavior;
+    stateBehaviors[State::Ready] = new ReadyStateBehavior();
+    stateBehaviors[State::Idle] = new BaseStateBehavior();
+    stateBehavior->onEnter();
 }
 
 void loop()
 {
-  M5.update();
-  if (M5.BtnA.wasPressed())
-  {
-    avatar.setExpression(expressions[idx]);
-    idx = (idx + 1) % expressionsSize;
-    // 口の開き具合をランダムに変える
-    avatar.setMouthOpenRatio(random(1, 10)*0.1);
-  }
+    if (currentState != nextState)
+    {
+        changeState();
+    }
+    stateBehavior->onUpdate();
 }
